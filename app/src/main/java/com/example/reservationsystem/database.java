c9bf1ba4010becc361a7f2bbf2e809df8aba1189
khans <khansachin33@gmail.com>
2023-04-29 13:38:03 +0530

    initial commit

---
+++ app/src/main/java/com/example/reservationsystem/database.java
@@ -0,0 +1,39 @@
+package com.example.reservationsystem;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class  database extends SQLiteOpenHelper  {
+    private static  final String dbname="signup.db";
+    public database(@Nullable Context context) {
+        super(context,dbname, null, 1);
+    }
+
+    @Override    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String q="create table users (id integer primary key autoincrement, name text ,username text ,password text)";
+        sqLiteDatabase.execSQL(q);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("drop table if exists users");
+        onCreate(sqLiteDatabase);
+    }
+    public boolean insert_data(String name,String username,String password){
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues c = new ContentValues();
+        c.put("name",name);
+        c.put("username",username);
+        c.put("password",password);
+        long r = db.insert("users",null,c);
+        if(r==-1) return false;
+        else
+            return true;
+    }
+
+}
+
